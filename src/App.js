889634148f3dcfa99ba8d3b595a9a1e8f42526fa
SubbarajuGitHub <122296930+SubbarajuGitHub@users.noreply.github.com>
2023-03-10 19:08:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
 
+import {Switch, Route} from 'react-router-dom'
+
 import LoginPage from './components/LoginPage'
 
 const App = () => (
   <>
-    <LoginPage />
+    <Switch>
+      <Route exact path="/ebank/login" component={LoginPage} />
+    </Switch>
   </>
 )
 
